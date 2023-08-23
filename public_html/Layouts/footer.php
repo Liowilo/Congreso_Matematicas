be3b46b7f1e9b482e9ea002f9ed5b44a131cc55d
RichiLM <142748349+RichiLM@users.noreply.github.com>
2023-08-23 00:42:58 -0600

    Cambio de cbbcongress a desarrollo en footer.php

--- public_html/Layouts/footer.php
+++ public_html/Layouts/footer.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-xl-3 col-lg-3 col-md-3 d-sm-block col-sm-12 mt-3 mb-2">
                 <p style="color:white">Desarrollado por:</p>
-                <a href="/cbbcongress/components/acercaDe/acercaDe.php"><img src="/cbbcongress/src/logoCbbW.png" alt="logoCbb" width="150" height="65"></a>
+                <a href="/desarrollo/components/acercaDe/acercaDe.php"><img src="/desarrollo/src/logoCbbW.png" alt="logoCbb" width="150" height="65"></a>
             </div>
             <div class="col-xl-6 col-lg-6 col-md-6 d-sm-block col-sm-12 mt-4">
                 <span class="texto-foo" style="color: White"> Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se mutile,  se cite la fuente completa y su dirección electrónica. 
