7c1c182daa8ddf4730e8a11b465b94df6070f8d1
RichiLM <142748349+RichiLM@users.noreply.github.com>
2023-08-23 00:32:12 -0600

    Actualización de la navegación a desarrollo

--- public_html/Layouts/nav.php
+++ public_html/Layouts/nav.php
@@ -2,7 +2,7 @@
 //session_start();
 //Verifica si ha iniciado sesión e instancia el modulo para consultar la foto del usuario.
 if (!empty($_SESSION['correoElectronico'])) {
-  require_once $_SERVER["DOCUMENT_ROOT"] . "/cbbcongress/modelo/traerFoto.php";
+  require_once $_SERVER["DOCUMENT_ROOT"] . "/desarrollo/modelo/traerFoto.php";
 }
 ?>
 
@@ -24,7 +24,7 @@ if (!empty($_SESSION['correoElectronico'])) {
     <div class="collapse navbar-collapse " id="navbarNavDropdown">
       <ul class="navbar-nav centrar sinEspacio">
         <li class="nav-item">
-          <a id="inicio" class="nav-link active a-nav mt-2" aria-current="page" href="/cbbcongress/index.php">Inicio</a>
+          <a id="inicio" class="nav-link active a-nav mt-2" aria-current="page" href="/desarrollo/index.php">Inicio</a>
         </li>
 
         <li class="nav-item dropdown ">
@@ -32,12 +32,12 @@ if (!empty($_SESSION['correoElectronico'])) {
             Información
           </a>
           <ul class="dropdown-menu super">
-            <li><a class="dropdown-item " href="/cbbcongress/src/convocatoria/XV.pdf" target="_blank">Convocatoria</a></li>
-            <li><a class="dropdown-item " href="/cbbcongress/components/registroInscripcion/inscripcion.php">Inscripción</a></li>
-            <li><a class="dropdown-item " href="/cbbcongress/components/Lugar/lugar.php">Lugar</a></li>
-            <li><a class="dropdown-item " href="/cbbcongress/components/registroCuotas/cuotas.php">Cuotas</a></li>
-            <li><a class="dropdown-item " href="/cbbcongress/components/GuiasYPlantillas/guias.php">Guias</a></li>
-            <li><a class="dropdown-item " href="/cbbcongress/components/acercaDe/acercaDe.php">Acerca de...</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/src/convocatoria/XV.pdf" target="_blank">Convocatoria</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/registroInscripcion/inscripcion.php">Inscripción</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/Lugar/lugar.php">Lugar</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/registroCuotas/cuotas.php">Cuotas</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/GuiasYPlantillas/guias.php">Guias</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/acercaDe/acercaDe.php">Acerca de...</a></li>
           </ul>
         </li>
 
@@ -46,9 +46,9 @@ if (!empty($_SESSION['correoElectronico'])) {
             Programa
           </a>
           <ul class="dropdown-menu super">
-            <li><a class="dropdown-item" href="/cbbcongress/components/programaMemorias/programaMemorias.php">Memorias</a></li>
-            <li><a class="dropdown-item " href="/cbbcongress/components/programa/programa.php">Programa</a></li>
-            <li><a class="dropdown-item" href="/cbbcongress/components/programaMagistrales/programaMagistral.php">Ponencias Magistrales</a></li>
+            <li><a class="dropdown-item" href="/desarrollo/components/programaMemorias/programaMemorias.php">Memorias</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/programa/programa.php">Programa</a></li>
+            <li><a class="dropdown-item" href="/desarrollo/components/programaMagistrales/programaMagistral.php">Ponencias Magistrales</a></li>
           </ul>
         </li>
 
@@ -57,9 +57,9 @@ if (!empty($_SESSION['correoElectronico'])) {
             Actividades
           </a>
           <ul class="dropdown-menu super">
-            <li><a class="dropdown-item " href="/cbbcongress/components/actividadesCategorias/categorias.php">Categorias</a></li>
-            <li><a class="dropdown-item " href="/cbbcongress/components/actividadesConcursoC/concursoC.php">Concurso de carteles</a></li>
-            <li><a class="dropdown-item " href="/cbbcongress/components/actividadesFechas/fechas.php">Fechas</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/actividadesCategorias/categorias.php">Categorias</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/actividadesConcursoC/concursoC.php">Concurso de carteles</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/actividadesFechas/fechas.php">Fechas</a></li>
 
           </ul>
         </li>
@@ -69,9 +69,9 @@ if (!empty($_SESSION['correoElectronico'])) {
             Organizadores
           </a>
           <ul class="dropdown-menu super">
-            <li><a class="dropdown-item " href="/cbbcongress/components/organizadoresComites/cOrganizador.php">Comite Organizador</a></li>
-            <li><a class="dropdown-item " href="/cbbcongress/components/organizadoresComites/cTecnico.php">Comite Tecnico</a></li>
-            <li><a class="dropdown-item " href="/cbbcongress/components/organizadoresComites/cEvaluador.php">Comite Evaluador</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/organizadoresComites/cOrganizador.php">Comite Organizador</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/organizadoresComites/cTecnico.php">Comite Tecnico</a></li>
+            <li><a class="dropdown-item " href="/desarrollo/components/organizadoresComites/cEvaluador.php">Comite Evaluador</a></li>
           </ul>
         </li>
         <?php
@@ -79,7 +79,7 @@ if (!empty($_SESSION['correoElectronico'])) {
         if (!empty($_SESSION['correoElectronico'])) {
         ?>
           <li class="nav-item">
-            <a class="nav-link a-nav mt-2" href="/cbbcongress/components/subirResumen/subirResumen.php">Registrar trabajos</a>
+            <a class="nav-link a-nav mt-2" href="/desarrollo/components/subirResumen/subirResumen.php">Registrar trabajos</a>
           </li>
         <?php }
 
@@ -105,9 +105,9 @@ if (!empty($_SESSION['correoElectronico'])) {
             //Hace un if para comprobar que la sesión está abierta y y muestra los elementos.
             if (!empty($_SESSION['correoElectronico'])) {
             ?>
-              <li><a class="dropdown-item" href="/cbbcongress/components/perfil/perfil.php">Mi Perfil</a></li>
-              <li><a class="dropdown-item" href="/cbbcongress/components/TrabajosRegistrados/trabajosRegistrados.php">Mis trabajos</a></li>
-              <li><a class="dropdown-item" href="/cbbcongress/modelo/cerrarSesion.php">Cerrar Sesión</a></li>
+              <li><a class="dropdown-item" href="/desarrollo/components/perfil/perfil.php">Mi Perfil</a></li>
+              <li><a class="dropdown-item" href="/desarrollo/components/TrabajosRegistrados/trabajosRegistrados.php">Mis trabajos</a></li>
+              <li><a class="dropdown-item" href="/desarrollo/modelo/cerrarSesion.php">Cerrar Sesión</a></li>
             <?php
             }
             ?>
@@ -115,8 +115,8 @@ if (!empty($_SESSION['correoElectronico'])) {
             //Hace un if para comprobar que la sesión está cerrada y muestra los elementos.
             if (empty($_SESSION['correoElectronico'])) {
             ?>
-              <li><a class="dropdown-item" href="/cbbcongress/components/inicioSesion/sesion.php">Iniciar Sesión</a></li>
-              <li><a class="dropdown-item" href="/cbbcongress/components/crearCuenta/cuenta.php">Registrarse</a></li>
+              <li><a class="dropdown-item" href="/desarrollo/components/inicioSesion/sesion.php">Iniciar Sesión</a></li>
+              <li><a class="dropdown-item" href="/desarrollo/components/crearCuenta/cuenta.php">Registrarse</a></li>
             <?php
             }
             ?>
@@ -125,7 +125,7 @@ if (!empty($_SESSION['correoElectronico'])) {
 
 
         <li class="nav-item">
-          <a class="nav-link " href="/cbbcongress/components/perfil/perfil.php" role="button" aria-expanded="false">
+          <a class="nav-link " href="/desarrollo/components/perfil/perfil.php" role="button" aria-expanded="false">
             <?php
             //Verifica si existe una sesion activa y muestra la foto del usuario
             if (!empty($_SESSION["correoElectronico"])) {
@@ -144,7 +144,7 @@ if (!empty($_SESSION['correoElectronico'])) {
             } else {
               //Verifica si existe una sesion cerrada y muestra la foto por defecto
               ?>
-              <img src="/cbbcongress/src/fotos_usuarios/picProfileNull.png" class="rounded-circle" alt="Foto de Perfil" width="40" height="40" vertical-align="middle">
+              <img src="/desarrollo/src/fotos_usuarios/picProfileNull.png" class="rounded-circle" alt="Foto de Perfil" width="40" height="40" vertical-align="middle">
             <?php
             }
             ?>
