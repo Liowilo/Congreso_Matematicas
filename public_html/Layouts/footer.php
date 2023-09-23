70e5efa4f99d28176468a9f5dd03f1fbdbb71f06
RichiLM <142748349+RichiLM@users.noreply.github.com>
2023-09-23 15:44:00 -0600

    Cambie logo de gecko systems en footer.php

--- public_html/Layouts/footer.php
+++ public_html/Layouts/footer.php
@@ -1,23 +1,28 @@
-
 <div class="container-fluid footer p-3 mx-0">
     <div class="container">
         <div class="row">
             <div class="col-xl-2 col-lg-2 col-md-2 d-sm-block col-sm-12 mt-3 mb-2">
-                <p style="color:white">Desarrollado por:</p>
-                <a href="/desarrollo/components/acercaDe/acercaDe.php"><img src="/desarrollo/src/logoCbbW.png" alt="logoCbb" width="150" height="65"></a>
+                <center>
+                    <p style="color:white">Desarrollado por:</p>
+                    <a href="/desarrollo/components/acercaDe/acercaDe.php"><img src="/desarrollo/src/logoCbbW.png" alt="logoCbb" width="150" height="65"></a>
+                </center>
             </div>
             <div class="col-xl-3 col-lg-3 col-md-3 d-sm-block col-sm-12 mt-3 mb-2">
-                <p style="color:white">Mantenimiento realizado por:</p>
-                <a href="/desarrollo/components/acercaDe/acercaDe.php"><img src="/desarrollo/src/GS2023.png" alt="logoGS" width="160" height="150"></a>
+                <center>
+                    <p style="color:white">Mantenimiento realizado por:</p>
+                    <a href="/desarrollo/components/acercaDe/acercaDe.php"><img src="/desarrollo/src/gecko1.png" alt="logoGS" width="120px" height="120px"></a>
+                </center>
             </div>
             <div class="col-xl-4 col-lg-4 col-md-4 d-sm-block col-sm-12 mt-4">
-                <span class="texto-foo" style="color: White"> Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se mutile,  se cite la fuente completa y su dirección electrónica. 
-                De otra forma requiere permiso previo por escrito de la institución. Sitio Web administrado por: Departamento de Matemáticas</span>
-                <span  class="texto-foo text-center"><br><script type="text/javascript">
-                copyright=new Date();
-                update=copyright.getFullYear();
-                document.write("© " + update + " " + "Todos los derechos reservados");
-                </script></span>
+                <span class="texto-foo" style="color: White"> Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se mutile, se cite la fuente completa y su dirección electrónica.
+                    De otra forma requiere permiso previo por escrito de la institución. Sitio Web administrado por: Departamento de Matemáticas</span>
+                <span class="texto-foo text-center"><br>
+                    <script type="text/javascript">
+                        copyright = new Date();
+                        update = copyright.getFullYear();
+                        document.write("© " + update + " " + "Todos los derechos reservados");
+                    </script>
+                </span>
             </div>
             <div class="col-xl-2 col-lg-2 col-md-2 d-sm-block col-sm-12 mt-4">
                 <p class="texto-foo">Telefonos</p>
@@ -28,4 +33,4 @@
             </div>
         </div>
     </div>
-</div> 
+</div>
