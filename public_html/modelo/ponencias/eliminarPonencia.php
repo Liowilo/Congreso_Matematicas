e54194cbb4683eaea1ad8af0a45982f59a09cbe4
BR105 <142694924+BR105@users.noreply.github.com>
2023-10-11 16:33:16 -0600

    Update eliminarPonencia.php
    Ya manda correo a los Autores y Coautores cuando se elimina una ponencia

--- public_html/modelo/ponencias/eliminarPonencia.php
+++ public_html/modelo/ponencias/eliminarPonencia.php
@@ -1,19 +1,117 @@
 <?php
+session_start();
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 require "../conexion.php";
+require "../../librerias/PHPMailer/src/Exception.php";
+require "../../librerias/PHPMailer/src/PHPMailer.php";
+require "../../librerias/PHPMailer/src/SMTP.php";
 
-$idPonencia=$_GET['id'];
+// Configuración de la clase PHPMailer para el envío de correo utilizando SMTP
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = "ssl";
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 465;
+$email = 'congresomatematicas15@gmail.com';
+$mail->Username = $email;
+$mail->Password = "rsdxrhmuwdcovefj";
 
-$consBorrarPonencia = "DELETE FROM ponencia WHERE id_ponencia='$idPonencia'";
-$resBorrarPonencia = mysqli_query($conexion, $consBorrarPonencia);
+// Obtener ID de la ponencia a eliminar
+$idPonencia = $_GET['id'];
 
+// Consulta para obtener id_usuario del autor
+$consultaAutor = "SELECT id_usuario_registra FROM ponencia WHERE id_ponencia='$idPonencia'";
+$resultadoAutor = mysqli_query($conexion, $consultaAutor);
+
+if ($filaAutor = mysqli_fetch_assoc($resultadoAutor)) {
+    $idUsuarioAutor = $filaAutor['id_usuario_registra'];
+    
+    // Consulta para obtener email_usuario del autor
+    $consultaEmailAutor = "SELECT email_usuario FROM usuario WHERE id_usuario='$idUsuarioAutor'";
+    $resultadoEmailAutor = mysqli_query($conexion, $consultaEmailAutor);
+    
+    if ($filaEmailAutor = mysqli_fetch_assoc($resultadoEmailAutor)) {
+        $autorEmail = $filaEmailAutor['email_usuario'];
+    }
+}
+
+// Consulta para obtener id_usuario de coautores
+$consultaCoautores = "SELECT id_usuario FROM usuario_colabora_ponencia WHERE id_ponencia='$idPonencia'";
+$resultadoCoautores = mysqli_query($conexion, $consultaCoautores);
+
+$coautoresEmails = array();
+while ($filaCoautor = mysqli_fetch_assoc($resultadoCoautores)) {
+    $idUsuarioCoautor = $filaCoautor['id_usuario'];
+    
+    // Consulta para obtener email_usuario de coautor
+    $consultaEmailCoautor = "SELECT email_usuario FROM usuario WHERE id_usuario='$idUsuarioCoautor'";
+    $resultadoEmailCoautor = mysqli_query($conexion, $consultaEmailCoautor);
+    
+    if ($filaEmailCoautor = mysqli_fetch_assoc($resultadoEmailCoautor)) {
+        $coautoresEmails[] = $filaEmailCoautor['email_usuario'];
+    }
+}
+
+// Consulta para obtener el título de la ponencia
+$consultaTitulo = "SELECT titulo_ponencia FROM ponencia WHERE id_ponencia='$idPonencia'";
+$resultadoTitulo = mysqli_query($conexion, $consultaTitulo);
+$titulo = "";
+
+if ($filaTitulo = mysqli_fetch_assoc($resultadoTitulo)) {
+    $titulo = $filaTitulo['titulo_ponencia'];
+}
+
+// Envío de correos electrónicos al autor y coautores
+$mail->addAddress($autorEmail); // Agregar al autor
+$mail->Body = "Se ha eliminado el trabajo '$titulo'.<br><br>Fecha: " . date('Y-m-d') . "<br><br>Atentamente,<br>El Comité Organizador del Evento<br>Por mi Raza Hablará el Espíritu";
+$mail->Subject = "Se ha eliminado un trabajo";
+$mail->isHTML(true);
+$mail->CharSet = 'UTF-8';
+$mail->From = "congresomatematicas15@gmail.com";
+$mail->FromName = "Congreso Internacional de Matemáticas";
+$mail->Send();
+$mail->ClearAddresses();
+
+// Envío de correos electrónicos a los coautores
+if (!empty($coautoresEmails)) {
+    foreach ($coautoresEmails as $destinatario) {
+        $mail->addAddress($destinatario);
+        $mail->Body = "Se ha eliminado el trabajo '$titulo'.<br><br>Fecha: " . date('Y-m-d') . "<br><br>Atentamente,<br>El Comité Organizador del Evento<br>Por mi Raza Hablará el Espíritu";
+        $mail->Subject = "Se ha eliminado un trabajo";
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+        $mail->From = "congresomatematicas15@gmail.com";
+        $mail->FromName = "Congreso Internacional de Matemáticas";
+        $mail->Send();
+        $mail->ClearAddresses(); // Limpiar las direcciones para el siguiente destinatario
+    }
+}
+
+if (isset($_GET['id'])) {
+    $idPonencia = $_GET['id'];
+    $consBorrarPonencia = "DELETE FROM ponencia WHERE id_ponencia='$idPonencia'";
+    $resBorrarPonencia = mysqli_query($conexion, $consBorrarPonencia);
+
+    if ($resBorrarPonencia) {
+        // La ponencia se eliminó correctamente
+        echo "La ponencia se eliminó correctamente.";
+    } else {
+        // Error al eliminar la ponencia
+        echo "Error al eliminar la ponencia: " . mysqli_error($conexion);
+    }
+} else {
+    // No se proporcionó un ID válido
+    echo "ID de ponencia no válido.";
+}
+
+$info = "ELIMINACIÓN de trabajo exitoso. Se ha enviado un correo electrónico al autor y a los coautores (si existen) con la información del registro.";
+echo $info;
 
 header("Location: ../../components/TrabajosRegistrados/trabajosRegistrados.php");
-
-
-//$fetchTrabajosRegistrados = mysqli_fetch_assoc($resTrabajosRegistrados);
-
-
-
-
-
 ?>
