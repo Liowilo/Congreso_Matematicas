7c7c2acf248bf343bb82cb5131c473c66158f704
TheApexSite <142762634+TheApexSite@users.noreply.github.com>
2023-10-11 18:36:01 -0600

    Diseño responsivo en ayudaPlantillas.php

--- public_html/components/AyudaPlantillas/ayudaPlantillas.php
+++ public_html/components/AyudaPlantillas/ayudaPlantillas.php
@@ -46,6 +46,7 @@ foreach (array_combine($privilegios, $estadoPrivilegio) as $valor => $estado) {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
             <link rel="stylesheet" href="../../styles.css">
             <link rel="stylesheet" href="../../Layouts/guiasPlantillas/ayudaPlantillas.css">
+            <link rel="stylesheet" href="../../components/cambiarDiseño/cambiarDiseño.css">
         </head>
 
         <body>
@@ -72,59 +73,70 @@ foreach (array_combine($privilegios, $estadoPrivilegio) as $valor => $estado) {
                                 unset($_SESSION['mensaje']); // Limpiar el mensaje
                             }
                             ?>
-                            <div class="container">
-                                <div class="table Cuerpo-texto">
-                                    <table>
-                                        <thead style="background-color: <?php echo $colorHex; ?> !important;">
-                                            <tr>
-                                                <th class="titulo-tabla" scope="col" width="50%"><span class="span-blanco">Plantillas Actuales</span></th>
-                                                <th class="titulo-tabla" scope="col" width="50%"><span class="span-blanco">Modificar Plantillas</span></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td width="50%">
-                                                    <span class="span-sub">Guía para autores</span>
-                                                    <br><br>
-                                                    Este documento detalla el proceso para presentar un trabajo de ponencia con las especificaciones requeridas para su aprobación
-                                                    <br><br><br><br>
-                                                    <div class="d-flex justify-content-center mt-4">
-                                                        <a href="../../src/GuiasYPlantillas/Guia_para_autores_2023.pdf" target="_blank" class="botones_descarga shadow py-1 px-4 mb-5 text-wrap btn text-btn">Descargar Guía</a>
-                                                    </div>
-                                                </td>
-                                                <td width="50%">
-                                                    <span class="span-sub">Guía para autores</span><br><br>
-                                                    <div class="d-flex justify-content-center mt-4">
-                                                        <form action="procesar_archivo_autores.php" method="POST" enctype="multipart/form-data">
-                                                            <input class="botones_descarga shadow py-1 px-4 mb-5 text-wrap btn text-btn" type="submit" name="submit1" value="Actualizar Archivo">
-                                                            <input type="file" name="nuevo_archivo1">
-                                                            <input type="hidden" name="archivo_a_reemplazar1" value="Guia_para_autores_2023.pdf"> <!-- Nombre del archivo a reemplazar -->
-                                                        </form>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td width="50%">
-                                                    <span class="span-sub">Plantilla para trabajos extensos</span><br><br>
-                                                    Los trabajos extensos deberán redactarse basándose en la plantilla de trabajos extensos. La única modalidad que requiere de un trabajo extenso es ponencia oral en formatos .doc o .docx Los archivos no deben exceder los (3 MB).
-                                                    <br><br><br><br>
-                                                    <div class="d-flex justify-content-center mt-4">
-                                                        <a href="../../src/GuiasYPlantillas/Plantilla_extenso_2023.docx" download="Plantilla_extenso_2023.docx" class="botones_descarga shadow py-1 px-4 mb-5 text-wrap btn text-btn">Descargar Plantilla</a>
-                                                    </div>
-                                                </td>
-                                                <td width="50%">
-                                                    <span class="span-sub">Plantilla para trabajos extensos</span><br><br>
-                                                    <div class="d-flex justify-content-center mt-4">
-                                                    <form action="procesar_archivo_extensos.php" method="POST" enctype="multipart/form-data">
-                                                            <input class="botones_descarga shadow py-1 px-4 mb-5 text-wrap btn text-btn" type="submit" name="submit2" value="Actualizar Archivo">
-                                                            <input type="file" name="nuevo_archivo2">
-                                                            <input type="hidden" name="archivo_a_reemplazar2" value="Plantilla_extenso_2023.docx"> <!-- Nombre del archivo a reemplazar -->
-                                                        </form>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                            <div class="cuadricula">
+                                    <div class="elemento-cuadricula">
+                                        <h4 style="background-color: <?php echo $colorHex; ?> !important;">Plantilla Actual Autores</h4>
+
+                                        <div class="elemento-cuadricula bd contenedor-plantillasA1">
+                                        <label style="font-size: 20px; font-weight: bold;">Guía para Autores</label>
+                                        <br><br>
+                                        <p>Este documento detalla el proceso para presentar un trabajo de ponencia con las especificaciones requeridas para su aprobación.</p>
+                                        <div class="d-flex justify-content-center mt-4">
+                                            <a href="../../src/GuiasYPlantillas/Guia_para_autores_2023.pdf" target="_blank" class="btn btn-primary" style="width: 80%; margin: 30px 0; background-color: #ebc961; font-size: 20px; font-weight: 600; border: none;">Descargar Guía</a>
+                                        </div> 
+                                        
+                                
+                                    </div>
+
+                                    </div>
+                                    <div class="elemento-cuadricula">
+                                        <h4 style="background-color: <?php echo $colorHex; ?> !important;">Modificar Plantilla Autores</h4>
+
+
+                                        <label style="font-size: 20px; font-weight: bold;">Guía para Autores</label>
+                                        <div class="elemento-cuadricula ">
+                                        <button type="button" data-bs-toggle="collapse" data-bs-target="#collapsePassword" aria-expanded="false" aria-controls="collapsePassword" class="btn btn-primary" type="button" style="width: 80%; margin: 30px 0; background-color: #ebc961; font-size: 20px; font-weight: 600; border: none;">Actualizar</button>
+                                        <div class="collapse" id="collapsePassword">
+                                            <div class="card card-body">
+                                                <form action="procesar_archivo_autores.php" method="post" enctype="multipart/form-data">
+                                                    <input type="file" name="nuevo_archivo1" class="form-control" >
+                                                    <input type="hidden" name="archivo_a_reemplazar1" value="Guia_para_autores_2023.pdf">
+                                                    <input type="submit" name="submit1" value="Actualizar" class="m-auto w-100 btn btn-sm btn-outline-warning">
+                                                </form>
+                                            </div>
+                                        </div>
+
+                                        
+                                    </div>
+                                    </div>
+
+                                    <div class="elemento-cuadricula">
+                                        <h4 style="background-color: <?php echo $colorHex; ?> !important;">Plantilla Actual Extensos</h4>
+                                        <div class="elemento-cuadricula bd contenedor-plantillasA1">
+                                        <label style="font-size: 20px; font-weight: bold;">Plantilla para trabajos extensos</label>
+                                        <br><br>
+                                        <p>Los trabajos extensos deberán redactarse basándose en la plantilla de trabajos extensos. La única modalidad que requiere de un trabajo extenso es ponencia oral en formatos .doc o .docx Los archivos no deben exceder los (3 MB).</p>
+                                        <div class="d-flex justify-content-center mt-4">
+                                            <a href="../../src/GuiasYPlantillas/Plantilla_extenso_2023.docx" target="_blank" class="btn btn-primary" style="width: 80%; margin: 30px 0; background-color: #ebc961; font-size: 20px; font-weight: 600; border: none;">Descargar Plantilla</a>
+                                        </div> 
+                                
+                                    </div>
+                                    </div>
+                                    <div class="elemento-cuadricula">
+                                        <h4 style="background-color: <?php echo $colorHex; ?> !important;">Modificar Plantilla Extensos</h4>
+                                        <label style="font-size: 20px; font-weight: bold;">Plantilla para trabajos extensos</label>
+                                        <div class="elemento-cuadricula ">
+                                        <button type="button" data-bs-toggle="collapse" data-bs-target="#collapseP" aria-expanded="false" aria-controls="collapseP" class="btn btn-primary" type="button" style="width: 80%; margin: 30px 0; background-color: #ebc961; font-size: 20px; font-weight: 600; border: none;">Actualizar</button>
+                                        <div class="collapse" id="collapseP">
+                                            <div class="card card-body">
+                                                <form action="procesar_archivo_extensos.php" method="post" enctype="multipart/form-data">
+                                                    <input type="file" name="nuevo_archivo2" class="form-control" >
+                                                    <input type="hidden" name="archivo_a_reemplazar2" value="Plantilla_extenso_2023.docx">
+                                                    <input type="submit" name="submit2" value="Actualizar" class="m-auto w-100 btn btn-sm btn-outline-warning">
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
