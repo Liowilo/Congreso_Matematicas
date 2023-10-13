9cdbaaaca5d513cdb88b0f274118ff8ff4c6e3fb
RichiLM <142748349+RichiLM@users.noreply.github.com>
2023-10-12 19:39:28 -0600

    Añadi conexion a cTecnico.php

--- public_html/components/organizadoresComites/cTecnico.php
+++ public_html/components/organizadoresComites/cTecnico.php
@@ -1,8 +1,15 @@
-<?php 
-    session_start();    
+<?php
+session_start();
+require "../../modelo/conexion.php";
+// Traer color automatizado
+$valorColor = "SELECT color_congreso FROM recursos_pagprin WHERE idRecurso = '1'";
+$color = mysqli_query($conexion, $valorColor);
+$rowColor = $color->fetch_assoc();
+$colorHex = $rowColor['color_congreso'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="shortcut icon" href="../../favicon.png">
     <meta charset="UTF-8">
@@ -14,91 +21,93 @@
     <link rel="stylesheet" href="./comites.css">
 
 </head>
+
 <body>
-<header class="fixed-top"> <!--------------MANDA A LLAMAR LA NAVBAR--------------->  
-    <?php 
-		require_once('../../Layouts/nav.php');
-	?>
-</header>
-<section style="margin-top: 250px;">
-<div class="container-fluid mt-5 mb-5">
-    <div class="row p-2"> 
-        <div class="col-xl-2 col-lg-2 col-md-1 d-none d-sm-block">
-        </div>
-<!----------CONTENEDOR PRINCIPAL----------->  
-        <div class="col-xl-10 col-lg-10 col-md-10 col-sm-12">
-        <!-----------Tabla------------> 
-            <div class= "container">
-            <h2 class="mb-4">Comité técnico</h2><!--------TITULO INTERNO------------> 
-                <table class="table table-striped mt-1" >
-                    <thead class="categorias">
-                        <tr>
-                            <th class="nombre py-2" scope="col">Nombre completo</th>
-                            <th class="institucion py-2" scope="col">Institución</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th class="nombre py-2" scope="row">M.D.A Clarisa Clemente Rodríguez</th>
-                            <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
-                        </tr>
-                        <tr>
-                            <th class="nombre py-2" scope="row ">M.C. Mayte Flores Pérez</th>
-                            <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
-                        </tr>
-                        <tr>
-                            <th class="nombre py-2" scope="row">M.C. Maricela Lara Martínez</th>
-                            <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
-                        </tr>
-                        <tr>
-                            <th class="nombre py-2" scope="row ">L.S.C. Liana López Pachecho</th>
-                            <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
-                        </tr>
-                        <tr>
-                            <th class="nombre py-2" scope="row">M. en CE. Domingo Márquez Ortega</th>
-                            <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
-                        </tr>
-                        <tr>
-                            <th class="nombre py-2" scope="row ">Ing. Guillermo Pérez Hernández</th>
-                            <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
-                        </tr>
-                        <tr>
-                            <th class="nombre py-2" scope="row">Ing. José Juan Rico Castro</th>
-                            <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
-                        </tr>
-                        <tr>
-                            <th class="nombre py-2" scope="row ">M.G.T.I. Rosalba Nancy Rosas Fonseca</th>
-                            <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
-                        </tr>
-                        <tr>
-                            <th class="nombre py-2" scope="row">M.V.Z. Alejandro Valdez Santamaría</th>
-                            <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
-                        </tr>
-                        <tr>
-                            <th class="nombre py-2" scope="row ">M. en C. María Guadalupe Vázquez Salazar</th>
-                            <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
-                        </tr>
-                    </tbody>
-                </table>
-                
+    <header class="fixed-top"> <!--------------MANDA A LLAMAR LA NAVBAR--------------->
+        <?php
+        require_once('../../Layouts/nav.php');
+        ?>
+    </header>
+    <section style="margin-top: 250px;">
+        <div class="container-fluid mt-5 mb-5">
+            <div class="row p-2">
+                <div class="col-xl-2 col-lg-2 col-md-1 d-none d-sm-block">
+                </div>
+                <!----------CONTENEDOR PRINCIPAL----------->
+                <div class="col-xl-10 col-lg-10 col-md-10 col-sm-12">
+                    <!-----------Tabla------------>
+                    <div class="container">
+                        <h2 class="mb-4">Comité técnico</h2><!--------TITULO INTERNO------------>
+                        <table class="table table-striped mt-1">
+                            <thead class="categorias">
+                                <tr style="background-color: <?php echo $colorHex; ?>;">
+                                    <th class="nombre py-2" scope="col">Nombre completo</th>
+                                    <th class="institucion py-2" scope="col">Institución</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th class="nombre py-2" scope="row">M.D.A Clarisa Clemente Rodríguez</th>
+                                    <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
+                                </tr>
+                                <tr>
+                                    <th class="nombre py-2" scope="row ">M.C. Mayte Flores Pérez</th>
+                                    <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
+                                </tr>
+                                <tr>
+                                    <th class="nombre py-2" scope="row">M.C. Maricela Lara Martínez</th>
+                                    <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
+                                </tr>
+                                <tr>
+                                    <th class="nombre py-2" scope="row ">L.S.C. Liana López Pachecho</th>
+                                    <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
+                                </tr>
+                                <tr>
+                                    <th class="nombre py-2" scope="row">M. en CE. Domingo Márquez Ortega</th>
+                                    <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
+                                </tr>
+                                <tr>
+                                    <th class="nombre py-2" scope="row ">Ing. Guillermo Pérez Hernández</th>
+                                    <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
+                                </tr>
+                                <tr>
+                                    <th class="nombre py-2" scope="row">Ing. José Juan Rico Castro</th>
+                                    <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
+                                </tr>
+                                <tr>
+                                    <th class="nombre py-2" scope="row ">M.G.T.I. Rosalba Nancy Rosas Fonseca</th>
+                                    <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
+                                </tr>
+                                <tr>
+                                    <th class="nombre py-2" scope="row">M.V.Z. Alejandro Valdez Santamaría</th>
+                                    <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
+                                </tr>
+                                <tr>
+                                    <th class="nombre py-2" scope="row ">M. en C. María Guadalupe Vázquez Salazar</th>
+                                    <td class="institucion py-2">Facultad de Estudios Superiores Cuautitlán</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+                <section>
+                    <?php
+                    require_once('../../Layouts/maps.php');
+                    ?>
+                </section>
             </div>
         </div>
-        <section>
-                <?php 
-		        require_once('../../Layouts/maps.php');
-	            ?>
-                </section>
-    </div>
-</div>
-            
-</section>
-<footer><!-----------CONTENEDOR PIE DE PAGINA------------>
-<?php 
-		require_once('../../Layouts/footer.php');
-	?>
-</footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-<script src="https://kit.fontawesome.com/c7b1d2a865.js" crossorigin="anonymous"></script>
+    </section>
+    <footer><!-----------CONTENEDOR PIE DE PAGINA------------>
+        <?php
+        require_once('../../Layouts/footer.php');
+        ?>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/c7b1d2a865.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
