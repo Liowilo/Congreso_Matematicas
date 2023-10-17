a56a2913a278ab1b4612c6678ef30ece3bdc1f40
RichiLM <142748349+RichiLM@users.noreply.github.com>
2023-10-16 21:42:57 -0600

    Elimine conexion traerCorreoCongreso.php
    Ya no es necesario porque todas las páginas tienen el archivo de conexión, si llega a haber un fallo el problema es esta línea XD

--- public_html/librerias/PHPMailer/src/traerCorreoCongreso.php
+++ public_html/librerias/PHPMailer/src/traerCorreoCongreso.php
@@ -3,7 +3,7 @@
     * Este modulo realiza la consulta del correo más reciente, es decir, el que tenga mayor 
     **/ 
 
-    include '../../../modelo/conexion.php';
+    // include '../../../modelo/conexion.php';
     //Trae el congreso más reciente
     $consCongreso="SELECT * FROM congreso WHERE id_congreso=(SELECT MAX(id_congreso) FROM congreso);";
     $resCongreso = mysqli_query($conexion, $consCongreso);
