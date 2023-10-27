c55cb978f9a8c8c91b98c0f89e3a5d2ff3ed0a58
RichiLM <142748349+RichiLM@users.noreply.github.com>
2023-10-27 14:45:18 -0600

    Corregimos mRGeneracionIdTrabajo.php

--- public_html/modelo/mRGeneracionIdTrabajo.php
+++ public_html/modelo/mRGeneracionIdTrabajo.php
@@ -14,51 +14,51 @@ require "traerCongresoActual.php";
     //Consulta el número de carteles del congreso
 
     
-    $consNumeroCartelesCongreso = "SELECT MAX(id_ponencia) AS max_id FROM ponencia WHERE id_congreso='$idCongreso'";
+    $consNumeroCartelesCongreso = "SELECT MAX(id_ponencia) AS max_id FROM ponencia WHERE substring(id_ponencia, 1, 2) = 'CA' AND id_congreso='$idCongreso'";
     $resNumeroCartelesCongreso = mysqli_query($conexion, $consNumeroCartelesCongreso);
     if (mysqli_num_rows($resNumeroCartelesCongreso) > 0) {
         $fetchNumeroCartelesCongreso = mysqli_fetch_assoc($resNumeroCartelesCongreso);
         $idPonenciaCartel = $fetchNumeroCartelesCongreso['max_id'];
     } else {
-        $idPonenciaCartel = 'CASM00000';
+        $idPonenciaCartel = 'CASM000';
     }
-    $numeroCartelesCongreso = (int)substr("$idPonenciaCartel", 4,-2) + 1;
+    $numeroCartelesCongreso = (int)substr("$idPonenciaCartel", 4) + 1;
     
     //Consulta el número de ponencias orales del congreso
     
-    $consNumeroPonenciasCongreso = "SELECT MAX(id_ponencia) AS max_id FROM ponencia WHERE id_congreso='$idCongreso'";
+    $consNumeroPonenciasCongreso = "SELECT MAX(id_ponencia) AS max_id FROM ponencia WHERE substring(id_ponencia, 1, 2) = 'PO' AND id_congreso='$idCongreso'";
     $resNumeroPonenciasCongreso = mysqli_query($conexion, $consNumeroPonenciasCongreso);
     if (mysqli_num_rows($resNumeroPonenciasCongreso) > 0) {
         $fetchNumeroPonenciasCongreso = mysqli_fetch_assoc($resNumeroPonenciasCongreso);
         $idPonenciaOral = $fetchNumeroPonenciasCongreso['max_id'];
     } else {
-        $idPonenciaOral = 'POSM00000';
+        $idPonenciaOral = 'POSM000';
     }
-    $numeroPonenciasCongreso = (int)substr("$idPonenciaOral", 4,-2) + 1;
+    $numeroPonenciasCongreso = (int)substr("$idPonenciaOral", 4) + 1;
     
     //Consulta el número de talleres del congreso
 
-    $consNumeroTalleresCongreso = "SELECT MAX(id_ponencia) AS max_id FROM ponencia WHERE id_congreso='$idCongreso'";
+    $consNumeroTalleresCongreso = "SELECT MAX(id_ponencia) AS max_id FROM ponencia WHERE substring(id_ponencia, 1, 2) = 'TA' AND id_congreso='$idCongreso'";
     $resNumeroTalleresCongreso = mysqli_query($conexion, $consNumeroTalleresCongreso);
     if (mysqli_num_rows($resNumeroTalleresCongreso) > 0) {
         $fetchNumeroTalleresCongreso = mysqli_fetch_assoc($resNumeroTalleresCongreso);
         $idPonenciaTaller = $fetchNumeroTalleresCongreso['max_id'];
     } else {
-        $idPonenciaTaller = 'TASM00000';
+        $idPonenciaTaller = 'TASM000';
     }
-    $numeroTalleresCongreso = (int)substr("$idPonenciaTaller", 4,-2) + 1;
+    $numeroTalleresCongreso = (int)substr("$idPonenciaTaller", 4) + 1;
    
     //Consulta el número de prototipos del congreso
 
-    $consNumeroPrototiposCongreso = "SELECT MAX(id_ponencia) AS max_id FROM ponencia WHERE id_congreso='$idCongreso'";
+    $consNumeroPrototiposCongreso = "SELECT MAX(id_ponencia) AS max_id FROM ponencia WHERE substring(id_ponencia, 1, 2) = 'PR' AND id_congreso='$idCongreso'";
     $resNumeroPrototiposCongreso = mysqli_query($conexion, $consNumeroPrototiposCongreso);
     if (mysqli_num_rows($resNumeroPrototiposCongreso) > 0) {
         $fetchNumeroPrototiposCongreso = mysqli_fetch_assoc($resNumeroPrototiposCongreso);
         $idPonenciaPrototipo = $fetchNumeroPrototiposCongreso['max_id'];
     } else {
-        $idPonenciaPrototipo = 'PRSM00000';
+        $idPonenciaPrototipo = 'PRSM000';
     }
-    $numeroPrototiposCongreso = (int)substr("$idPonenciaPrototipo", 4,-2) + 1;
+    $numeroPrototiposCongreso = (int)substr("$idPonenciaPrototipo", 4) + 1;
     /** 
     *******************************************************************************************************
     *******************************************************************************************************
