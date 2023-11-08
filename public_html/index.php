60f83c3f0fa6fd3d886fc065081ecd6555c1a3d3
Kevin García <318002822@cuautitlan.unam.mx>
2023-11-08 14:18:47 -0600

    Se agrego un aviso sobre la incompatibilidad de Firefox
    Se agrego un aviso sobre la incompatibilidad de Firefox en index y al iniciar sesión.
    
    Co-Authored-By: Cesared09 <130081984+Cesared09@users.noreply.github.com>

--- public_html/index.php
+++ public_html/index.php
@@ -12,13 +12,19 @@ require 'modelo/conexion.php';
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title id="titulo">Congreso FESC</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-	<link rel="stylesheet" href="./styles.css">
+	<link rel="stylesheet" href="./styles.css?v=1">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <style>
+
+    </style>
 </head>
 
 <body>
+	<div class="mascara-blur" id="mascara-blur"></div>
+
 	<header class="fixed-top">
 		<?php
 		$index = true;
@@ -26,6 +32,22 @@ require 'modelo/conexion.php';
 		?>
 	</header>
 
+    <div class="container mt-4 content-flag" id="content-flag">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="important-ad">
+                    <button type="button" class="close-button" id="close-flag">
+                        <i class="fas fa-times px-2"></i>
+                    </button>
+                    <h4 class="alert-heading"><strong>Anuncio Importante:</strong></h4>
+					<h4>Incompatibilidad con Mozilla Firefox</h4>
+                    <p><br><br>Por favor, ten en cuenta que nuestra página web no es compatible actualmente con Mozilla Firefox. Te sugerimos utilizar otro navegador como Google Chrome, Microsoft Edge u Opera para acceder a nuestros servicios. Lamentamos las molestias y agradecemos tu comprensión.</p>
+                    <p><br><br>Gracias.</p>
+                    <p><strong>CISEyAM</strong></p>
+                </div>
+            </div>
+        </div>
+	</div>
 
 	<section style="margin-top: 210px;">
 
@@ -46,6 +68,10 @@ require 'modelo/conexion.php';
 		require_once('Layouts/maps.php');
 		?>
 	</section>
+
+
+
+
 
 	<footer>
 		<?php
