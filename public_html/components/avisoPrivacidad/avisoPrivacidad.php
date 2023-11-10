121e6e5494676bd4f5f6d5c17b6ac5d559471de7
Kevin García <78277985+KevinGarc8714@users.noreply.github.com>
2023-11-10 16:05:24 -0600

    Actualizacion Aviso de privacidad
    Se agregaron cambios menores y el correo que se actualiza con la tabla de la BD.

--- public_html/components/avisoPrivacidad/avisoPrivacidad.php
+++ public_html/components/avisoPrivacidad/avisoPrivacidad.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require "../../modelo/conexion.php";
+require "../../components/avisoPrivacidad/correoCongreso.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,54 +37,55 @@ require "../../modelo/conexion.php";
         <div class="container mt-5 mb-5">
             <h2 class="mb-3 text-center" style="margin-top: 10px;">Aviso de privacidad</h2>
             <div class="row mt-5">
-                <div class="col-lg-8 offset-lg-2">
-                    <p class="justified-text reduced-line-height">
-                        La Facultad de Estudios Superiores (FES) Cuautitlán, Campus 4, ubicada en Carretera Cuautitlán-Teoloyucan Km. 2.5, San Sebastian Xhala, 54714 Cuautitlán Izcalli, Estado de México, recaba datos personales y es responsable del tratamiento que se les dé.
-                    </p>
-                    <p class="justified-text reduced-line-height">
-                        <strong>Datos personales recabados:</strong>
-                    </p>
-                    <ul class="reduced-line-height">
-                        <li>Nombre y apellidos: Para identificación en el proceso de registro.</li>
-                        <li>RFC (Registro Federal de Contribuyentes): Para temas administrativos, en caso de ser requerido.</li>
-                        <li>Correo electrónico: Utilizado para comunicaciones relacionadas con el congreso.</li>
-                        <li>Número de teléfono: Opcional, para contacto adicional en caso de necesidad.</li>
-                        <li>Foto de perfil autógrafa: Para identificación en el perfil del asistente.</li>
-                    </ul>
-                    <p class="justified-text reduced-line-height">
-                        <strong>Finalidades del tratamiento de datos:</strong>
-                    </p>
-                    <p class="justified-text reduced-line-height">
-                        Estos datos serán utilizados con el objetivo de:
-                    </p>
-                    <ul class="reduced-line-height">
-                        <li>Facilitar el registro y la participación en el Congreso Internacional sobre la Enseñanza y Aplicación de las Matemáticas.</li>
-                        <li>Contactar a los participantes para comunicaciones relacionadas con el evento.</li>
-                        <li>Identificar a los asistentes en el entorno digital del congreso a través de la foto de perfil autografiada.</li>
-                    </ul>
-                    <p class="justified-text reduced-line-height">
-                        <strong>Cookies y Web Beacons:</strong>
-                    </p>
-                    <p class="justified-text reduced-line-height">
-                        El sitio web del congreso puede utilizar cookies y web beacons con el fin de mejorar la experiencia del usuario y obtener información estadística.
-                    </p>
-                    <p class="justified-text reduced-line-height">
-                        <strong>Ejercicio de derechos ARCO:</strong>
-                    </p>
-                    <p class="justified-text reduced-line-height">
-                        Los participantes tienen derecho a acceder, rectificar, cancelar u oponerse al uso de sus datos personales. Para ejercer estos derechos, podrán contactar a la organización del congreso a través de la dirección de correo electrónico designada para este propósito.
-                    </p>
-                    <p class="justified-text reduced-line-height">
-                        <strong>Modificaciones al Aviso de Privacidad:</strong>
-                    </p>
-                    <p class="justified-text reduced-line-height">
-                        Cualquier modificación o actualización a este aviso de privacidad estará disponible en el sitio web oficial del Congreso Internacional sobre la Enseñanza y Aplicación de las Matemáticas (CISEyAM).
-                    </p>
-                    <p class="justified-text reduced-line-height">
-                        <strong>Fecha última actualización:</strong> 07/11/2023
-                    </p>
-                </div>
-            </div>
+            <div class="col-lg-8 offset-lg-2">
+  <div id="aviso-privacidad">
+  <p class="justified-text reduced-line-height">
+      La Facultad de Estudios Superiores (FES) Cuautitlán, ubicada en Carretera Cuautitlán-Teoloyucan Km. 2.5, San Sebastián Xhala, 54714 Cuautitlán Izcalli, Estado de México, recaba datos personales y es responsable del tratamiento que se les dé.
+    </p>
+    
+    <strong>Datos personales recabados:</strong>
+    <ul class="reduced-line-height">
+      <li>Nombre y apellidos: Para identificación en el proceso de registro.</li>
+      <li>RFC (Registro Federal de Contribuyentes): Para temas administrativos, en caso de ser requerido.</li>
+      <li>Correo electrónico: Utilizado para enviar información y constancias relacionadas con el congreso.</li>
+      <li>Número de teléfono: Opcional, para contacto adicional en caso de necesidad.</li>
+      <li>Foto de perfil autografiada: Para identificación en el perfil del asistente.</li>
+      <li>Datos académicos: Información académica relevante para el Congreso.</li>
+      <li>Datos laborales: Información laboral relevante para el Congreso.</li>
+    </ul>
+    
+    <strong>Finalidades del tratamiento de datos:</strong>
+    <ul class="reduced-line-height">
+      <li>Facilitar el registro y la participación en el Congreso Internacional sobre la Enseñanza y Aplicación de las Matemáticas.</li>
+      <li>Enviar información y constancias relacionadas con el congreso.</li>
+    </ul>
+    
+    <strong>Cookies y Web Beacons:</strong>
+    <p class="justified-text reduced-line-height">
+      El sitio web del congreso puede utilizar cookies y web beacons con el fin de mejorar la experiencia del usuario y obtener información estadística.
+    </p>
+    
+    <strong>Ejercicio de derechos ARCO:</strong>
+    <p class="justified-text reduced-line-height">
+      Los participantes tienen derecho a acceder, rectificar, cancelar u oponerse al uso de sus datos personales. Para ejercer estos derechos, podrán contactar a la organización del congreso a través de la dirección de correo electrónico designada para este propósito. 
+    </p>
+    <p class="justified-text reduced-line-height"><em><?php echo $correoCongreso; ?></em></p>
+    
+    <p class="justified-text reduced-line-height">
+      En caso de solicitar acceso, deberá registrarse en la página.
+    </p>
+
+    <strong>Modificaciones al Aviso de Privacidad:</strong>
+    <p class="justified-text reduced-line-height">
+      Cualquier modificación o actualización a este aviso de privacidad estará disponible en el sitio web oficial del Congreso Internacional sobre la Enseñanza y Aplicación de las Matemáticas (CISEyAM).
+    </p>
+    
+    <p class="justified-text reduced-line-height">
+      <strong>Fecha última actualización:</strong> 10/11/2023
+    </p>
+  </div>
+</div>
+
         </div>
     </section>
 
---
+++ public_html/components/avisoPrivacidad/correoCongreso.php
@@ -0,0 +1,11 @@
+<?php
+//include '../../../modelo/conexion.php';
+// Trae el congreso más reciente
+$consCongreso = "SELECT * FROM congreso WHERE id_congreso=(SELECT MAX(id_congreso) FROM congreso);";
+$resCongreso = mysqli_query($conexion, $consCongreso);
+$fetchCongreso = mysqli_fetch_assoc($resCongreso);
+
+// Traer usuario y contraseña
+$correoCongreso = $fetchCongreso['correo_congreso'];
+$hashContra = $fetchCongreso['contra_congreso'];
+?>
