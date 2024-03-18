57e272742c765491cd0bea745619ef0406026b9a
RichiLM <richar12.rlom@gmail.com>
2024-03-17 21:49:26 -0600

    Añadi indicaciones en los reportes

--- public_html/Layouts/TablasDeReportes/extensosAprobados.php
+++ public_html/Layouts/TablasDeReportes/extensosAprobados.php
@@ -1,4 +1,16 @@
 <div id="extensosAprobados" class="scroll table-responsive border border-success border-opacity-10 rounded pt-2 px-2 pb-5 mt-4" style="max-height: 800px;">
+    <?php
+    //En caso de que el autor tenga ponencias
+    require "../../modelo/trabajosReportes.php";
+
+    if($etapaTrabajo == 'RESUMEN'){
+        echo '<h5>Los resumenes ACEPTADOS son los que tienen pendiente enviar su EXTENSO</h5>';
+    }
+
+    if($etapaTrabajo == 'EXTENSO'){
+        echo '<h5>Los extensos ACEPTADOS pasan automaticamente a la etapa EXTENSOS FINALES pendientes por evaluar</h5>';
+    }
+    ?>
     <table class="table">
         <tr class="head-table">
             <th scope="col">Id Ponencia</th>
@@ -15,39 +27,39 @@
         <tbody>
             <tr>
                 <?php
-
-                //En caso de que el autor tenga ponencias
-                require "../../modelo/trabajosReportes.php";
-                // require "../../modelo/trabajosReportesTMP.php";
-
-                while ($fetchPonenciasRegistradas = mysqli_fetch_assoc($ejecucionTMPAprobado)) {
-                    $idPonencia = $fetchPonenciasRegistradas['id_ponencia'];
-                    $tituloPonencia = $fetchPonenciasRegistradas['titulo_ponencia'];
-                    $nombrePonente = $fetchPonenciasRegistradas['ponente'];
-                    $emailPonente = $fetchPonenciasRegistradas['email_usuario'];
-                    $descRevision = $fetchPonenciasRegistradas['descripcion_revision'];
-                    $estatusRev = $fetchPonenciasRegistradas['estatus_revision'];
-                    $fechaRegistroPonencia = $fetchPonenciasRegistradas['fecha'];
-                    //Da formato de fecha
-                    $date = date_create($fechaRegistroPonencia);
-                    $fechaRegistroPonenciaFormato = date_format($date, "Y/m/d H:i");
-                    $nombreEvaluador = $fetchPonenciasRegistradas['Evaluador'];
-                    $emailEvaluador = $fetchPonenciasRegistradas['correo_evaluador'];
-
-                ?>
-                    <td class="text-wrap text-uppercase"><?php echo $idPonencia; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $tituloPonencia; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $nombrePonente; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $emailPonente; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $descRevision; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $estatusRev; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $fechaRegistroPonenciaFormato; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $nombreEvaluador; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $emailEvaluador; ?></td>
-            </tr>
-        <?php
+                if(mysqli_fetch_assoc($ejecucionTMPAprobado) > 0){
+                    while ($fetchPonenciasRegistradas = mysqli_fetch_assoc($ejecucionTMPAprobado)) {
+                        $idPonencia = $fetchPonenciasRegistradas['id_ponencia'];
+                        $tituloPonencia = $fetchPonenciasRegistradas['titulo_ponencia'];
+                        $nombrePonente = $fetchPonenciasRegistradas['ponente'];
+                        $emailPonente = $fetchPonenciasRegistradas['email_usuario'];
+                        $descRevision = $fetchPonenciasRegistradas['descripcion_revision'];
+                        $estatusRev = $fetchPonenciasRegistradas['estatus_revision'];
+                        $fechaRegistroPonencia = $fetchPonenciasRegistradas['fecha'];
+                        //Da formato de fecha
+                        $date = date_create($fechaRegistroPonencia);
+                        $fechaRegistroPonenciaFormato = date_format($date, "Y/m/d H:i");
+                        $nombreEvaluador = $fetchPonenciasRegistradas['Evaluador'];
+                        $emailEvaluador = $fetchPonenciasRegistradas['correo_evaluador'];
+    
+                    ?>
+                        <td class="text-wrap text-uppercase"><?php echo $idPonencia; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $tituloPonencia; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $nombrePonente; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $emailPonente; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $descRevision; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $estatusRev; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $fechaRegistroPonenciaFormato; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $nombreEvaluador; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $emailEvaluador; ?></td>
+                </tr>
+            <?php
+                    }
+                } else {
+                    echo '<td colspan="9"><h5 class="text-center">No se encontraron trabajos aceptados en la etapa <b>'. $etapaTrabajo .'</b></h5></td>';
                 }
-        ?>
+            ?>
+                
         </tbody>
     </table>
 </div>
--- public_html/Layouts/TablasDeReportes/extensosPendientesEvaluar.php
+++ public_html/Layouts/TablasDeReportes/extensosPendientesEvaluar.php
@@ -1,4 +1,9 @@
 <div id="extensosPendientesEvaluar" class="scroll table-responsive border border-success border-opacity-10 rounded pt-2 px-2 pb-5 mt-4" style="max-height: 800px;">
+    <?php
+    //En caso de que el autor tenga ponencias
+    require "../../modelo/trabajosReportes.php";
+
+    ?>
     <table class="table">
         <tr class="head-table">
             <th scope="col">Id Ponencia</th>
@@ -14,40 +19,39 @@
         </thead>
         <tbody>
             <tr>
-                <?php
-
-                //En caso de que el autor tenga ponencias
-                require "../../modelo/trabajosReportes.php";
-                // require "../../modelo/trabajosReportesTMP.php";
-
-                while ($fetchPonenciasRegistradas = mysqli_fetch_assoc($ejecucionTMPPendienteEvaluar)) {
-                    $idPonencia = $fetchPonenciasRegistradas['id_ponencia'];
-                    $tituloPonencia = $fetchPonenciasRegistradas['titulo_ponencia'];
-                    $nombrePonente = $fetchPonenciasRegistradas['ponente'];
-                    $emailPonente = $fetchPonenciasRegistradas['email_usuario'];
-                    $descRevision = $fetchPonenciasRegistradas['descripcion_revision'];
-                    $estatusRev = $fetchPonenciasRegistradas['estatus_revision'];
-                    $fechaRegistroPonencia = $fetchPonenciasRegistradas['fecha'];
-                    //Da formato de fecha
-                    $date = date_create($fechaRegistroPonencia);
-                    $fechaRegistroPonenciaFormato = date_format($date, "Y/m/d H:i");
-                    $nombreEvaluador = $fetchPonenciasRegistradas['Evaluador'];
-                    $emailEvaluador = $fetchPonenciasRegistradas['correo_evaluador'];
-
-                ?>
-                    <td class="text-wrap text-uppercase"><?php echo $idPonencia; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $tituloPonencia; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $nombrePonente; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $emailPonente; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $descRevision; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $estatusRev; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $fechaRegistroPonenciaFormato; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $nombreEvaluador; ?></td>
-                    <td class="text-wrap text-uppercase"><?php echo $emailEvaluador; ?></td>
-            </tr>
-        <?php
+            <?php
+                if(mysqli_fetch_assoc($ejecucionTMPPendienteEvaluar) > 0){
+                    while ($fetchPonenciasRegistradas = mysqli_fetch_assoc($ejecucionTMPPendienteEvaluar)) {
+                        $idPonencia = $fetchPonenciasRegistradas['id_ponencia'];
+                        $tituloPonencia = $fetchPonenciasRegistradas['titulo_ponencia'];
+                        $nombrePonente = $fetchPonenciasRegistradas['ponente'];
+                        $emailPonente = $fetchPonenciasRegistradas['email_usuario'];
+                        $descRevision = $fetchPonenciasRegistradas['descripcion_revision'];
+                        $estatusRev = $fetchPonenciasRegistradas['estatus_revision'];
+                        $fechaRegistroPonencia = $fetchPonenciasRegistradas['fecha'];
+                        //Da formato de fecha
+                        $date = date_create($fechaRegistroPonencia);
+                        $fechaRegistroPonenciaFormato = date_format($date, "Y/m/d H:i");
+                        $nombreEvaluador = $fetchPonenciasRegistradas['Evaluador'];
+                        $emailEvaluador = $fetchPonenciasRegistradas['correo_evaluador'];
+    
+                    ?>
+                        <td class="text-wrap text-uppercase"><?php echo $idPonencia; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $tituloPonencia; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $nombrePonente; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $emailPonente; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $descRevision; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $estatusRev; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $fechaRegistroPonenciaFormato; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $nombreEvaluador; ?></td>
+                        <td class="text-wrap text-uppercase"><?php echo $emailEvaluador; ?></td>
+                </tr>
+            <?php
+                    }
+                } else {
+                    echo '<td colspan="9"><h5 class="text-center">No se encontraron trabajos pendeintes por evaluar en la etapa <b>'. $etapaTrabajo .'</b></h5></td>';
                 }
-        ?>
+            ?>
         </tbody>
     </table>
 </div>
