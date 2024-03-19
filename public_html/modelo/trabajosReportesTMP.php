b85beb89fb508e26325fa56b651b1939169eedc6
RichiLM <richar12.rlom@gmail.com>
2024-03-18 18:51:18 -0600

    Automatice congreso en tabla temporal

--- public_html/modelo/trabajosReportesTMP.php
+++ public_html/modelo/trabajosReportesTMP.php
@@ -1,5 +1,6 @@
 <?php
 require "conexion.php";
+require "traerCongresoActual.php";
 
 $nombreTabla = 'tmp';
 
@@ -12,12 +13,12 @@ if (mysqli_num_rows($resultado) > 0) {
     mysqli_query($conexion, "DROP TABLE $nombreTablaEscapada");
 }
 
-$tablaTemporal = '
+$tablaTemporal = "
 CREATE TABLE tmp
 SELECT substring(p.id_ponencia,8,3) as num,
     p.id_ponencia,
     p.titulo_ponencia,
-    concat(u.nombres_usuario," ",u.apellidos_usuario) as Ponente,
+    concat(u.nombres_usuario,' ',u.apellidos_usuario) as Ponente,
     u.email_usuario, 
     r.descripcion_revision,
     MAX(r.fecha_revision) as fecha,
@@ -29,11 +30,11 @@ FROM ponencia p,
     usuario u
 WHERE p.id_ponencia=ur.id_ponencia 
     AND ur.id_revision_ponencia=r.id_revision
-    AND p.id_congreso="16"
+    AND p.id_congreso='$idCongreso'
     AND p.id_tipo_ponencia=2
     AND p.id_usuario_registra = u.id_usuario
 GROUP BY p.id_ponencia
-';
+";
 
 mysqli_query($conexion, $tablaTemporal);
 
